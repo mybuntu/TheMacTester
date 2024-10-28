199d2fcd9d22b24d560e49dd909650a3668fa3c2
mybuntu <101842543+mybuntu@users.noreply.github.com>
2024-10-28 18:45:55 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
 # Run si t'as la flemme mais ces commandes marchent mieux executées une par une. 
 #!/bin/bash
 python3 -m venv venv
+source venv/bin/activate
 pip3 install --upgrade pip
 pip3 install -r requirements.txt
-source venv/bin/activate
+
