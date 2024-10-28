b262475dc8ddd506cdb14a633ec98ca2d5bf378f
mybuntu <101842543+mybuntu@users.noreply.github.com>
2024-10-28 18:43:53 +0100

    Delete st.sh

--- st.sh
+++
@@ -1,6 +0,0 @@
-# Run si t'as la flemme mais ces commandes marchent mieux executées une par une. 
-#!/bin/bash
-python3 -m venv venv
-pip3 install --upgrade pip
-pip3 install -r requirements.txt
-source venv/bin/activate
