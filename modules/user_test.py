15e88f2296288329d365d8920cc51d2252a040e6
mybuntu <101842543+mybuntu@users.noreply.github.com>
2024-11-01 14:34:41 +0100

    Update user_test.py

--- modules/user_test.py
+++ modules/user_test.py
@@ -1,9 +1,11 @@
-# The GUIless Mac Tester   
-## by Jules David
+# # The GUIless Mac Tester   
+# ## by Jules David
 import tkinter as tk
+from tkinter import simpledialog
 import os
 
-def user_test():
+# LISTE DES QUESTIONS
+def user_test(output_file):
     issues_detected = {}
 
     questions = [
@@ -13,7 +15,7 @@ def user_test():
         "Ventilateurs ?",
         "Wifi/Airport ?",
         "Bluetooth ?",
-        "clavier ?",
+        "Clavier ?",
         "Bouton Power ?",
         "Empreinte Touch ID ?",
         "Indicateur de charge ?",
@@ -22,27 +24,29 @@ def user_test():
         "Témoin de mise en veille ?",
         "Touchbar ?",
         "Rétroéclairage Clavier ?",
-        "Capteur de luminiosité ?"
+        "Capteur de luminosité ?"
     ]
 
     os.system('cls' if os.name == 'nt' else 'clear')
 
     for question in questions:
-        answer = input(f"([O]k(Fonctionnel)/[D]effectueux): (Enter => O)\n{question}").strip().lower()
-        if answer in ["d", "D"]:
+        answer = simpledialog.askstring("Test Utilisateur", f"{question}\n([F]onctionnel)/[D]éfectueux/[N]e possède pas): (Enter => [F]onctionnel)")
+        if answer and answer.lower() == "d":
             issues_detected[question] = "Défectueux"
+        elif answer and answer.lower() == "n":
+            issues_detected[question] = "Ne possède pas"
         else:
             issues_detected[question] = "Fonctionnel"
 
     os.system('cls' if os.name == 'nt' else 'clear')
 
     # Stocker les résultats dans resultats.txt
-    with open("output/resultats.txt", "a") as file:
+    with open(output_file, "a", encoding='utf-8') as file:
         file.write("=== RÉSULTATS DES TESTS UTILISATEUR ===\n")
         for question, status in issues_detected.items():
             file.write(f"{question.split(' au niveau du ')[-1]}: {status}\n")
         file.write("\n")
-        
+
     # Demander l'état global de l'ordinateur
     etat_options = {
         "1": "Neuf",
@@ -52,12 +56,11 @@ def user_test():
         "5": "Usé"
     }
 
-    etat_global = input("ÉTAT GLOBAL DE L'ORDINATEUR:\nA(1)/ Neuf\nB(2)/ Très bon état\nC(3)/ Bon état\nD(4)/ Correct\nE(5)/ Usé\nSélectionnez une option 1, 2, 3, 4, 5: ").strip().lower()
-
+    etat_global = simpledialog.askstring("État Global", "ÉTAT GLOBAL DE L'ORDINATEUR:\n1/ Neuf\n2/ Très bon état\n3/ Bon état\n4/ Correct\n5/ Usé\nSélectionnez une option (1-5): ")
     etat_result = etat_options.get(etat_global, "Non spécifié")
 
     # Ajouter l'état global à la fin du fichier resultats.txt
-    with open("output/resultats.txt", "a") as file:
+    with open(output_file, "a", encoding='utf-8') as file:
         file.write("=== ÉTAT GLOBAL DE L'ORDINATEUR ===\n")
         file.write(f"État: {etat_result}\n\n")
 
