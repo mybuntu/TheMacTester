4b150bc86c60e8e19ae7fc3df590dc8a0dd25629
mybuntu <101842543+mybuntu@users.noreply.github.com>
2024-12-10 12:11:45 +0100

    Update user_test.py

--- modules/user_test.py
+++ modules/user_test.py
@@ -39,6 +39,8 @@ def user_test(output_file):
             issues_detected[question] = "Défectueux"
         elif answer and answer.lower() == "n":
             issues_detected[question] = "Ne possède pas"
+        elif answer and answer.lower() == "a":
+            issues_detected[question] = "A REMPLACER"
         else:
             issues_detected[question] = "Fonctionnel"
 
